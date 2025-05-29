7f704836191ae630ff2b991781ee22c70e4a106a
Adarsh <adarshdixit910@gmail.com>
2025-05-29 23:55:37 +0530

    connect the db

--- src/db.js
+++ src/db.js
@@ -1,25 +1,18 @@
 const mongoose = require("mongoose");
-const { MONGO_URI } = require("./config/secrets");
-// require("dotenv").config(); // Load environment variables
+
+const MONGO_URI = "mongodb://localhost:27017/AD";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      (module.exports = {
-        secretKey: "ad_backend",
-        MONGO_URI:
-          "mongodb+srv://<adarsh>:<Adarsh@123>@cluster0.axnmstw.mongodb.net/",
-      }),
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-    console.log("MongoDB Connected Successfully! ✅");
+    console.log("Connected to Local MongoDB Successfully! ✅");
   } catch (error) {
     console.error("MongoDB Connection Error:", error);
-    process.exit(1); // Exit if connection fails
+    process.exit(1);
   }
 };
 
